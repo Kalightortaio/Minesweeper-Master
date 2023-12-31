94feb74f7e20908b13c640f7737615edd6e157b5
Krishna Kokatay <kokataykrishna@gmail.com>
2023-12-31 14:06:55 +0000

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{fontSize: 24}}>Hello World!</Text>
     </View>
   );
 }
