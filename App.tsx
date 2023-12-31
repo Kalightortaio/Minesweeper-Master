700c0fc6ca0cf8ca699b295392024dde2d45119f
Krishna Kokatay <kokataykrishna@gmail.com>
2023-12-31 14:34:08 +0000

    basic grid implemented

--- App.tsx
+++ App.tsx
@@ -1,9 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+
+const numColumns = 9;
+const numRows = 9;
+
+const windowWidth = Dimensions.get('window').width;
 
 export default function App() {
+  const renderCell = (rowIndex: number, columnIndex: number) => {
+    return (
+      <TouchableOpacity
+        key={`${rowIndex}-${columnIndex}`}
+        style={styles.cell}
+        onPress={() => console.log('Cell pressed', rowIndex, columnIndex)}
+      >
+        <Text>0</Text>
+      </TouchableOpacity>
+    )
+  }
+
+  const renderGrid = () => {
+    const grid = [];
+    for (let row = 0; row < numRows; row++) {
+      const columns = [];
+      for (let col = 0; col < numColumns; col++) {
+        columns.push(renderCell(row, col));
+      }
+      grid.push(
+        <View
+          key={`row-${row}`}
+          style={styles.row}
+        >
+          {columns}
+        </View>
+      )
+    }
+    return grid;
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={{fontSize: 24}}>Hello World!</Text>
+      {renderGrid()}
     </View>
   );
 }
@@ -11,8 +47,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  cell: {
+    width: windowWidth / numColumns,
+    height: windowWidth / numColumns,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: '#000',
   },
 });
