637c1e7c46e5de342c38e0736e45c8a9cb3714da
Krishna Kokatay <kokataykrishna@gmail.com>
2024-01-09 02:52:56 +0000

    Implemented the Zoomable Component and managed state between zoom and cells

--- App.tsx
+++ App.tsx
@@ -1,16 +1,19 @@
 import * as Font from 'expo-font';
 import { useEffect, useState } from 'react';
-import { View, StatusBar, AppState } from 'react-native';
+import { View, StatusBar } from 'react-native';
+import Zoomable from './src/components/Zoomable';
 import Interface from './src/components/Interface';
 import Cell from './src/components/Cell';
 import { cellSize, numColumns, numRows } from './src/Constants';
 import { styles } from './src/Styles';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
   const [fontsLoaded, setFontsLoaded] = useState(false);
   const [isFirstPress, setIsFirstPress] = useState(true);
   const [timer, setTimer] = useState(0);
   const [timerIntervalId, setTimerIntervalId] = useState<NodeJS.Timeout | null>(null);
+  const [isPanOrPinchActive, setPanOrPinchActive] = useState(false);
 
   useEffect(() => {
     StatusBar.setHidden(true);
@@ -73,6 +76,7 @@ export default function App() {
           rowIndex={row}
           columnIndex={col}
           onFirstPress={handleFirstPress}
+          isPanOrPinchActive={isPanOrPinchActive}
         />
       );
     }
@@ -84,14 +88,21 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.interface}>
-        {fontsLoaded && <Interface timer={timer}/>}
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <View style={styles.interface}>
+          {fontsLoaded && <Interface timer={timer}/>}
+        </View>
+        <View style={styles.grid}>
+          <Zoomable 
+            style={{ overflow: 'hidden', zIndex: 0 }} 
+            setPanOrPinchActive={setPanOrPinchActive}
+          >
+            {gridLines}
+            {rows}
+          </Zoomable>
+        </View>
       </View>
-      <View style={styles.grid}>
-        {gridLines}
-        {rows}
-      </View>
-    </View>
+    </GestureHandlerRootView >
   );
 }
