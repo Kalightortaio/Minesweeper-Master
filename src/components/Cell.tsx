d1251a76f8153df867af859ddbddf052e3b27304
Krishna Kokatay <kokataykrishna@gmail.com>
2024-01-09 02:51:48 +0000

    Replaced TouchableWithoutFeedback with Gesture methods

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,42 +1,55 @@
 import React, { useState } from "react";
-import { TouchableWithoutFeedback, Text, Vibration, View } from "react-native";
+import { Text, Vibration, View } from "react-native";
 import { computeStyle } from "../Styles";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+
 
 interface CellProps {
     rowIndex: number,
     columnIndex: number,
     onFirstPress: () => void,
+    isPanOrPinchActive: boolean,
 }
 
-function Cell({ rowIndex, columnIndex, onFirstPress }: CellProps) {
+function Cell({ rowIndex, columnIndex, onFirstPress, isPanOrPinchActive }: CellProps) {
     const [isRevealed, setIsRevealed] = useState(false);
     const [isFlagged, setIsFlagged] = useState(false);
 
-    function handlePress() {
-        if (!isRevealed && !isFlagged) {
-            setIsRevealed(true);
-            onFirstPress();
-        }
-        console.log('Cell pressed', rowIndex, columnIndex)
-    }
+    const tapGesture = Gesture.Tap()
+        .runOnJS(true)
+        .onStart(() => {
+            if (!isPanOrPinchActive) {
+                if (!isRevealed && !isFlagged) {
+                    setIsRevealed(true);
+                    onFirstPress();
+                    console.log('Cell pressed', rowIndex, columnIndex);
+                }
+            }
+        });
 
-    function handleLongPress() {
-        if (!isRevealed) {
-            setIsFlagged(!isFlagged);
-            Vibration.vibrate(50);
-            onFirstPress();
-        }
-        console.log('Cell long pressed', rowIndex, columnIndex)
-    }
+    const longPressGesture = Gesture.LongPress()
+        .runOnJS(true)
+        .minDuration(200)
+        .onStart(() => {
+            if (!isPanOrPinchActive) {
+                if (!isRevealed) {
+                    setIsFlagged(!isFlagged);
+                    onFirstPress();
+                    Vibration.vibrate(80);
+                    console.log('Cell long pressed', rowIndex, columnIndex);
+                }
+            }
+        });
 
     return (
-        <TouchableWithoutFeedback onPress={handlePress} onLongPress={handleLongPress}>
-            <View style={computeStyle('cell', { isRevealed, isFlagged }) }>
+        <GestureDetector gesture={Gesture.Exclusive(tapGesture, longPressGesture)}>
+            <View style={computeStyle('cell', { isRevealed, isFlagged })}>
                 <Text>
+                    {/* Cell content */}
                 </Text>
             </View>
-        </TouchableWithoutFeedback>
-    )
+        </GestureDetector>
+    );
 }
 
 export default Cell;
