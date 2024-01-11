a808b6045d37f7dd2a117fe9614b337868c359d3
Krishna Kokatay <kokataykrishna@gmail.com>
2024-01-11 22:27:14 +0000

    Added CellStateProps

--- src/Types.ts
+++ src/Types.ts
@@ -1,1 +1,10 @@
-export type StyleKeys = 'container' | 'interface' | 'grid' | 'gridLineX' | 'gridLineY' | 'gridRow' | 'cell' | 'isRevealed' | 'isFlagged';
+export type StyleKeys = 'container' | 'interface' | 'grid' | 'gridLineX' | 'gridLineY' | 'gridRow' | 'cell' | 'isRevealed' | 'isFlagged' | 'isMine';
+export interface CellStateProps {
+    rowIndex: number;
+    columnIndex: number;
+    isRevealed: boolean;
+    isFlagged: boolean;
+    isMine: boolean;
+    isCorner: boolean;
+    neighbors: number;
+}
