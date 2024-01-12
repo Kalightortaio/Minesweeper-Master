767cdf72fd7135fae2f4464903749a5b3aa75136
Krishna Kokatay <kokataykrishna@gmail.com>
2024-01-12 11:41:06 +0000

    Added board buttons

--- src/Constants.ts
+++ src/Constants.ts
@@ -9,3 +9,4 @@ export const gridPadding = (screenWidth * 0.05);
 export const cellSize = ((screenWidth - (2 * (gridPadding))) / numColumns);
 export const boardWidth = numColumns * cellSize;
 export const boardHeight = numRows * cellSize;
+export const buttonLength = (2 * (cellSize - borderWidth)) - (screenWidth * 0.02);
