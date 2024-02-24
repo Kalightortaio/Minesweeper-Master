511424b86844315763f11d51c265c04bc6b1b004
Krishna Kokatay <kokataykrishna@gmail.com>
2024-02-24 02:31:46 +0000

    Removed splash screen component

--- App.tsx
+++ App.tsx
@@ -1,48 +1,58 @@
 import * as Font from 'expo-font';
-import React, { useEffect, useState } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import React, { useCallback, useEffect, useState } from 'react';
 import { StatusBar } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { FontsLoadedContext } from './src/Constants';
-import Splash from './src/screens/Splash';
 import MainMenu from './src/screens/MainMenu';
 import ClassicMode from './src/screens/ClassicMode';
 import HighScores from './src/screens/HighScores';
 import ChallengeMode from './src/screens/ChallengeMode';
 import Settings from './src/screens/Settings';
 
+SplashScreen.preventAutoHideAsync();
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [appReady, setAppReady] = useState(false);
   const [fontsLoaded, setFontsLoaded] = useState(false);
 
   useEffect(() => {
-    StatusBar.setHidden(true);
-    const minimumSplash = 1000;
-    (async () => {
-      const fontLoadPromise = Font.loadAsync({
-        'DSEG': require('./assets/fonts/DSEG.ttf'),
-        'MINESWEEPER': require('./assets/fonts/MINE-SWEEPER.ttf'),
-      });
-      await Promise.all([
-        fontLoadPromise,
-        new Promise(resolve => setTimeout(resolve, minimumSplash))
-      ]);
-      setFontsLoaded(true);
-    })();
+    async function prepare() {
+      try {
+        StatusBar.setHidden(true);
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({
+          'DSEG': require('./assets/fonts/DSEG.ttf'),
+          'MINESWEEPER': require('./assets/fonts/MINE-SWEEPER.ttf'),
+        });
+        await new Promise(resolve => setTimeout(resolve, 1000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setFontsLoaded(true);
+        setAppReady(true);
+      }
+    }
+    prepare();
     return () => {
       StatusBar.setHidden(false);
     };
   }, []);
 
+  const onLayoutRootView = useCallback(async () => {
+    if (appReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appReady]);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <FontsLoadedContext.Provider value={fontsLoaded}>
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="SplashScreen">
-            {fontsLoaded ? (
-              <>
+        {appReady && <NavigationContainer onReady={onLayoutRootView}>
+           <Stack.Navigator initialRouteName="MainMenu">
               <Stack.Screen
                 name="MainMenu" 
                 component={MainMenu}
@@ -68,16 +78,8 @@ export default function App() {
                 component={Settings} 
                 options={{ headerShown: true }}  
               />
-              </>
-            ) : (
-                <Stack.Screen
-                  name="SplashScreen"
-                  component={Splash}
-                  options={{ headerShown: false }}
-                />
-            )}
           </Stack.Navigator>
-        </NavigationContainer>
+        </NavigationContainer>}
       </FontsLoadedContext.Provider>
     </GestureHandlerRootView >
   );
